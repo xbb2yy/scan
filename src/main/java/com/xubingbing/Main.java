074ed673bfddb5911801e1b745822b18cb1a8d5c
xubingbing <xubb@gdca.com.cn>
2019-04-28 09:38:45 +0800

    :zap:

--- src/main/java/com/xubingbing/Main.java
+++ src/main/java/com/xubingbing/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("/app.fxml"));
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
-        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/ico.jpg")));
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/scan.ico")));
         primaryStage.show();
     }
 
