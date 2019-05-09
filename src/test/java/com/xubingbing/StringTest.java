4547f30509a7dfd8196b9009e90ceebe05e63b5d
xubingbing <xubb@gdca.com.cn>
2019-05-09 15:05:53 +0800

    :arrow_down:

--- src/test/java/com/xubingbing/StringTest.java
+++ src/test/java/com/xubingbing/StringTest.java
@@ -2,14 +2,8 @@ package com.xubingbing;
 
 import org.junit.Test;
 
-import java.time.LocalTime;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
-import java.util.Random;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 public class StringTest {
 
